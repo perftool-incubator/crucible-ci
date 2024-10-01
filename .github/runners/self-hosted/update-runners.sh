1b6e8093bc6a24f5c938d71309ca8d04bff742bf
Karl Rister <krister@redhat.com>
2024-10-01 12:45:14 -0500

    separate the configuration for self-hosted runners into privileged and unprivileged

--- .github/runners/self-hosted/update-runners.sh
+++ .github/runners/self-hosted/update-runners.sh
@@ -4,28 +4,40 @@ pushd $(dirname $0) > /dev/null
 
 runner_user="manager"
 
-if [ ! -e runners.conf ]; then
-    echo "ERROR: Could not find runners.conf"
-    exit 1
-fi
+for runner_type in "privileged" "unprivileged"; do
+    if pushd ${runner_type}; then
+	echo "runner_type=${runner_type}"
 
-for runner_ip in $(cat runners.conf); do
-    echo "runner_ip=${runner_ip}"
+	if [ ! -e runners.conf ]; then
+	    echo "ERROR: Could not find runners.conf"
+	    exit 1
+	fi
 
-    echo "syncing actions-runner-hooks:"
-    rsync -av --progress --stats --delete actions-runner-hooks ${runner_user}@${runner_ip}:
+	for runner_ip in $(cat runners.conf); do
+	    echo "runner_ip=${runner_ip}"
 
-    echo "listing actions-runner-hooks:"
-    ssh ${runner_user}@${runner_ip} "ls -l ~/actions-runner-hooks"
+	    echo "syncing actions-runner-hooks:"
+	    rsync -av --progress --stats --delete actions-runner-hooks ${runner_user}@${runner_ip}:
 
-    echo "synching .env:"
-    rsync -av --progress --stats .env ${runner_user}@${runner_ip}:~/actions-runner/.env
+	    echo "listing actions-runner-hooks:"
+	    ssh ${runner_user}@${runner_ip} "ls -l ~/actions-runner-hooks"
 
-    echo "displaying .env:"
-    ssh ${runner_user}@${runner_ip} "cat ~/actions-runner/.env"
+	    echo "synching .env:"
+	    rsync -av --progress --stats .env ${runner_user}@${runner_ip}:~/actions-runner/.env
 
-    echo "restarting the runner:"
-    ssh ${runner_user}@${runner_ip} "sudo reboot"
+	    echo "displaying .env:"
+	    ssh ${runner_user}@${runner_ip} "cat ~/actions-runner/.env"
 
-    echo "##########################################################"
+	    echo "restarting the runner:"
+	    ssh ${runner_user}@${runner_ip} "sudo reboot"
+
+	    echo "##########################################################"
+	done
+
+	popd
+
+	echo "**********************************************************"
+    else
+	echo "ERROR: Failed to pushd to ${runner_type}"
+    fi
 done
