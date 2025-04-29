542acd4904668a0ba119fff56d97c943056bf213
Karl Rister <krister@redhat.com>
2025-04-29 08:17:01 -0500

    add handling for toolbox logged die

--- .github/runners/self-hosted/unprivileged/actions-runner-hooks/job-started.sh
+++ .github/runners/self-hosted/unprivileged/actions-runner-hooks/job-started.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+toolbox_logged_die_filename="/tmp/toolbox_logged_die.txt"
+if [ -e "${toolbox_logged_die_filename}" ]; then
+    echo "Found ${toolbox_logged_die_filename}"
+    rm -v ${toolbox_logged_die_filename}
+fi
+
 # cleanup the workspace
 if [ -n "${GITHUB_WORKSPACE}" ]; then
     if pushd "${GITHUB_WORKSPACE}"; then
