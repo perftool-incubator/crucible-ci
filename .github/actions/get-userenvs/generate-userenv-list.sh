d8dd11a47d28462e83fc6adf0dee29df720e08c1
Karl Rister <krister@redhat.com>
2025-07-23 09:51:54 -0500

    update the get-userenvs action to intelligently return only modified userenvs for certain rickshaw PRs
    - if the rickshaw PR is only adding or modifying specific userenvs
      then it only needs to test those specific userenvs rather than all
      testable userenvs
    
    - the action is being modified to check for the existence of rickshaw
      repository history and then to use that history to determine which
      userenvs should be returned
    
    - if rickshaw repository history is not available OR changes to
      rickshaw beyond the userenv directory exist then the previous
      behavior of testing all testable userenvs is maintained
    
    - the core-crucible-ci workflow also needs to have repository history
      made available for rickshaw when it is the CI target for this to
      work so that change is included as well

--- .github/actions/get-userenvs/generate-userenv-list.sh
+++ .github/actions/get-userenvs/generate-userenv-list.sh
@@ -5,6 +5,19 @@
 rickshaw_directory=${1}
 userenv_filter=${2}
 
+# if proper history is available then checks will be performed to see
+# if only a subset (ie. those modified) of userenvs need to be tested.
+# a subset will only be tested if the only modifications are to
+# userenvs, any modification outside of the userenv scope will force
+# all testable userenvs to be included
+#
+# assume that if the proper history is not available that is because
+# the code being examined is not from a submitted PR but from a
+# repositories merged branch (ie. upstream or a specific release) --
+# meaning test all testable userenvs
+diff_cmd_validate="git log HEAD^1"
+diff_cmd="git diff --name-only HEAD^1 HEAD"
+
 if pushd ${rickshaw_directory}; then
     excludes=""
     excludes_file="userenvs/ci-excludes.txt"
@@ -13,33 +26,103 @@ if pushd ${rickshaw_directory}; then
             excludes+="${userenv} "
         done < ${excludes_file}
     else
+        # historical excludes prior to creation of the excludes file
         excludes="stream8-flexran rhel-ai"
     fi
 
-    userenvs=$(find userenvs/ -maxdepth 1 -name '*.json' -type f | sed -e 's|userenvs/||' -e 's|\.json||')
+    all_testable_userenvs=0
+    if ${diff_cmd_validate} > /dev/null 2>&1; then
+        # history is available -- this must be a rickshaw repository PR
+        echo "Rickshaw history is available"
+
+        rickshaw_files_changed=$(${diff_cmd})
+        echo "Rickshaw files changed:"
+        echo "${rickshaw_files_changed}"
+        non_userenv_files_changed=$(echo "${rickshaw_files_changed}" | grep -v "^userenvs/" | wc -l)
+        if [ ${non_userenv_files_changed} -gt 0 ]; then
+            # the normal / historic behavior will be preserved
+            all_testable_userenvs=1
+            echo "Non userenv changes are present so reverting to normal behavior"
+        fi
+    else
+        # history is not available
+        echo "Rickshaw history is not available"
+
+        all_testable_userenvs=1
+    fi
+
+    if [ ${all_testable_userenvs} -eq 1 ]; then
+        # get the list of all testable userenvs
+        userenvs=$(find userenvs/ -maxdepth 1 -name '*.json' -type f)
+    else
+        # only generate the list of modified / created userenvs since
+        # no other rickshaw changes are present
+        userenvs=$(${diff_cmd} | grep "^userenvs/")
+    fi
+    userenvs=$(echo "${userenvs}" | sed -e 's|userenvs/||' -e 's|\.json||')
+
+    echo "Initial list of userenvs (pre-exlusion): ${userenvs[@]}"
+
     # Discard excluded envs
+    echo "Excluding userenvs:"
     for ex in ${excludes[@]}; do
+        echo "Discarding userenv ${ex} as it is not testable"
         new=( "${userenvs[@]/$ex}" )
         userenvs=$new
     done
+
+    echo "Post exclusion list of userenvs: ${userenvs[@]}"
+
     userenvs_json="["
 
-    case "${userenv_filter}" in
-        "all"|"minimal")
-            userenvs_json+="\"default\","
-            ;;
-    esac
+    if [ ${all_testable_userenvs} -eq 1 ]; then
+        # this is the normal / historic behavior
+        echo "Adding userenvs through normal filtering process"
 
-    case "${userenv_filter}" in
-        "all"|"unique")
-            for userenv in ${userenvs}; do
-                userenvs_json+="\"${userenv}\","
-            done
-            ;;
-    esac
+        case "${userenv_filter}" in
+            "all"|"minimal")
+                userenvs_json+="\"default\","
+                echo "Adding 'default' userenv"
+                ;;
+        esac
+
+        case "${userenv_filter}" in
+            "all"|"unique")
+                for userenv in ${userenvs}; do
+                    userenvs_json+="\"${userenv}\","
+                    echo "Adding '${userenv}' userenv"
+                done
+                ;;
+        esac
+    else
+        # this is the new behavior where only modified / created
+        # userenvs are being tested when no other rickshaw changes are
+        # present
+        echo "Adding userenvs through rickshaw userenv PR process"
+
+        userenv_count=0
+        for userenv in ${userenvs}; do
+            userenvs_json+="\"${userenv}\","
+            echo "Adding '${userenv}' userenv"
+            (( userenv_count += 1 ))
+        done
+
+        echo "Total userenvs added: ${userenv_count}"
+
+        if [ ${userenv_count} -eq 0 ]; then
+            # if we have reached this point then it is likely that a
+            # situation has occurred where there no userenvs to test
+            # -- for example the current rickshaw PR is only removing
+            # a userenv -- so generate a single default userenv to
+            # satisify testing requirements
+            echo "Adding 'default' userenv since no other userenvs were added"
+            userenvs_json+="\"default\","
+        fi
+    fi
 
     userenvs_json=$(echo "${userenvs_json}" | sed -e "s/,$//")
     userenvs_json+="]"
+
     echo "userenvs_json=${userenvs_json}"
     if [ -n "${GITHUB_OUTPUT}" ]; then
         echo "userenvs=${userenvs_json}" >> ${GITHUB_OUTPUT}
